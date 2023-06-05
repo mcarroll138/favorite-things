d24597c22932ddf6322cc7f4d78a7247f7a301c2
Michael <mcarroll138@gmail.com>
2023-06-05 16:10:59 -0700

    Update JS, add ul, debug
    Co-authored-by: Suzanne Schuber suzanneschuber@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,30 @@ window.onload = function () {
     form.onsubmit = function (event) {
         const houseWifeInput = document.getElementById("houseWife").value;
         const mountainRangeInput = document.getElementById("mountainRange").value;
-        console.log("houseWifeInput" + houseWifeInput)
         event.preventDefault();
 
         const bothThings = [houseWifeInput, mountainRangeInput];
         console.log(bothThings);
+
+        console.log(bothThings[0]);
+
+        const reverse = bothThings.reverse();
+        console.log(reverse)
+
+        const ul = document.createElement("ul");
+        ul.setAttribute('id', 'favs');
+        document.body.appendChild(ul);
+
+        let li1 = document.createElement('li');
+        let li2 = document.createElement('li');
+        li1.append(houseWifeInput);
+        li2.append(mountainRangeInput);
+        document.getElementById('favs').append(li1, li2);
+
+
     }
 
 
 }
+
+//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
